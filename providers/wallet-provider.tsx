f7074d375399923e22fb40746ab67d71061deec0
lou1s <lou1svuong.dev@gmail.com>
2024-12-28 04:17:00 +0700

    Add WalletProviders component for integrating wallet functionality with Westend AssetHub support

---
+++ providers/wallet-provider.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import * as React from 'react'
+import { RainbowKitProvider, getDefaultWallets, getDefaultConfig, darkTheme } from '@rainbow-me/rainbowkit'
+import { trustWallet, ledgerWallet } from '@rainbow-me/rainbowkit/wallets'
+import { kairos } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider, http } from 'wagmi'
+import { defineChain } from 'viem'
+
+const { wallets } = getDefaultWallets()
+
+export const westendAssetHub = defineChain({
+  id: 420420421,
+  name: 'Westend AssetHub',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Westend',
+    symbol: 'WND'
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://westend-asset-hub-eth-rpc.polkadot.io'],
+      webSocket: ['wss://westend-asset-hub-eth-rpc.polkadot.io']
+    }
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'https://assethub-westend.subscan.io' }
+  },
+  contracts: {
+    multicall3: {
+      address: '0x5545dec97cb957e83d3e6a1e82fabfacf9764cf1',
+      blockCreated: 10174702
+    }
+  }
+})
+
+const config = getDefaultConfig({
+  appName: '0xpenGuildWTF',
+  projectId: '455a9939d641d79b258424737e7f9205',
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [trustWallet, ledgerWallet]
+    }
+  ],
+  chains: [westendAssetHub],
+  transports: {
+    [westendAssetHub.id]: http()
+  },
+  ssr: true
+})
+
+const queryClient = new QueryClient()
+
+export function WalletProviders({ children }: { children: any }) {
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          initialChain={0}
+          showRecentTransactions={true}
+          theme={darkTheme({
+            accentColor: '#ff8800',
+            accentColorForeground: 'white',
+            borderRadius: 'none'
+          })}
+          locale='en-US'
+        >
+          {mounted && children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
