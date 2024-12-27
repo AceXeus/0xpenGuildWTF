f74f5403d945b32826d851601b6f1240441be0d3
lou1s <lou1svuong.dev@gmail.com>
2024-12-28 04:17:21 +0700

    Add IntegrationsSection component to display integrations with responsive layout

---
+++ containers/landing/integrations.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Integrations } from '@/components/ui/integrations'
+
+export function IntegrationsSection() {
+  return (
+    <div className='relative z-10 h-[400px] w-full overflow-hidden bg-background'>
+      <Integrations />
+    </div>
+  )
+}
