5a8e3b9a0eee124fcd12a5e7f51c6f37b4de804c
lou1s <lou1svuong.dev@gmail.com>
2024-12-29 02:31:04 +0700

    Add OurStory component to showcase the project's background and narrative

---
+++ app/our-story/page.tsx
@@ -0,0 +1,8 @@
+export default function OurStory() {
+  return (
+    <section className='mt-12 border border-border p-4 lg:mt-14 lg:p-16'>
+      <h2 className='mb-4 text-2xl font-bold md:text-3xl'>Our Story</h2>
+      <p>wat da fak is that ?? that's 0xpenGuildWTF</p>
+    </section>
+  )
+}
