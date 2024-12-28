1216acba755d72d2f804bb1c73e4b1fb9382a902
lou1s <lou1svuong.dev@gmail.com>
2024-12-29 02:30:44 +0700

    Refactor Tailwind CSS configuration to enhance animations and keyframes. Added new accordion animations and improved existing marquee animations for better UI transitions.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
   theme: {
   	extend: {
   		colors: {
-			highlight: "#ff8800",
+  			highlight: '#ff8800',
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
@@ -58,20 +58,46 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-		animation: {
-			marquee: "marquee var(--duration) linear infinite",
-			"marquee-vertical": "marquee-vertical var(--duration) linear infinite",
-		},
-		keyframes: {
-			marquee: {
-			  from: { transform: "translateX(0)" },
-			  to: { transform: "translateX(calc(-100% - var(--gap)))" },
-			},
-			"marquee-vertical": {
-			  from: { transform: "translateY(0)" },
-			  to: { transform: "translateY(calc(-100% - var(--gap)))" },
-			},
-		  },
+  		animation: {
+  			marquee: 'marquee var(--duration) linear infinite',
+  			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		keyframes: {
+  			marquee: {
+  				from: {
+  					transform: 'translateX(0)'
+  				},
+  				to: {
+  					transform: 'translateX(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'marquee-vertical': {
+  				from: {
+  					transform: 'translateY(0)'
+  				},
+  				to: {
+  					transform: 'translateY(calc(-100% - var(--gap)))'
+  				}
+  			},
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
